a7c05ac98150ef210dc42773b96980b3bee7eedb
Andy Street <andrews@fb.com>
2014-11-15 12:10:19 +0000

    Initial commit

---
+++ src/edudrone/EduDroneDriver.java
@@ -0,0 +1,11 @@
+package edudrone;
+
+import de.yadrone.base.ARDrone;
+
+public class EduDroneDriver {
+
+  public static void main(String[] args) {
+    ARDrone drone = new ARDrone();
+    drone.start();
+  }
+}
